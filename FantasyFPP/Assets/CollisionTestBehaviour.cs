4dfa76f73f6fa77153d0dd64e86b46b131e53a65
macibor811@student.polsl.pl <macibor811@student.polsl.pl>
2019-05-08 16:03:27 +0200

    Add demo project

---
+++ FantasyFPP/Assets/CollisionTestBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTestBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var vertices = GetColliderVertexPositions(this.gameObject);
+        foreach( Vector3 point in vertices)
+        {
+            Debug.Log(point.x + ", " + point.y + ", " + point.z);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var objects = GameObject.FindGameObjectsWithTag("Obbs");
+        var collider = this.GetComponent<BoxCollider>();
+        Obb obb = ObbCollisionDetection.ToObb(collider);
+
+        //gameObject.GetComponent<Renderer>().material.color = Color.blue;
+
+        foreach (GameObject obj in objects)
+        {
+            if (obj != this)
+            {
+                if (ObbCollisionDetection.Intersects(obb, ObbCollisionDetection.ToObb(obj.GetComponent<BoxCollider>())))
+                {
+                    gameObject.GetComponent<Renderer>().material.color = Color.green;
+                }
+                else
+                {   // rend.material.shader = Shader.Find("_Color");
+                    gameObject.GetComponent<Renderer>().material.color = Color.red;
+                }
+            }
+           
+          
+        }
+
+    }
+
+    public Vector3[] GetColliderVertexPositions(GameObject obj)
+    {
+        BoxCollider b = obj.GetComponent<BoxCollider>(); //retrieves the Box Collider of the GameObject called obj
+        Vector3[] vertices = new Vector3[8];
+        vertices[0] = obj.transform.TransformPoint(b.center + new Vector3(-b.size.x, -b.size.y, -b.size.z) * 0.5f);
+        vertices[1] = obj.transform.TransformPoint(b.center + new Vector3(b.size.x, -b.size.y, -b.size.z) * 0.5f);
+        vertices[2] = obj.transform.TransformPoint(b.center + new Vector3(b.size.x, -b.size.y, b.size.z) * 0.5f);
+        vertices[3] = obj.transform.TransformPoint(b.center + new Vector3(-b.size.x, -b.size.y, b.size.z) * 0.5f);
+        vertices[4] = obj.transform.TransformPoint(b.center + new Vector3(-b.size.x, b.size.y, -b.size.z) * 0.5f);
+        vertices[5] = obj.transform.TransformPoint(b.center + new Vector3(b.size.x, b.size.y, -b.size.z) * 0.5f);
+        vertices[6] = obj.transform.TransformPoint(b.center + new Vector3(b.size.x, b.size.y, b.size.z) * 0.5f);
+        vertices[7] = obj.transform.TransformPoint(b.center + new Vector3(-b.size.x, b.size.y, b.size.z) * 0.5f);
+       
+
+        return vertices;
+    }
+}
---
+++ FantasyFPP/Assets/ObbCollisionDetection.cs
@@ -0,0 +1,109 @@
+﻿
+using UnityEngine;
+
+public class Obb
+{
+    public readonly Vector3[] Vertices;
+    public readonly Vector3 Right;
+    public readonly Vector3 Up;
+    public readonly Vector3 Forward;
+
+    public Obb(BoxCollider collider)
+    {
+        var rotation = collider.transform.rotation;
+
+        Vertices = new Vector3[8];
+        Vertices[0] = collider.transform.TransformPoint(collider.center + new Vector3(-collider.size.x, -collider.size.y, -collider.size.z) * 0.5f);
+        Vertices[1] = collider.transform.TransformPoint(collider.center + new Vector3(collider.size.x, -collider.size.y, -collider.size.z) * 0.5f);
+        Vertices[2] = collider.transform.TransformPoint(collider.center + new Vector3(collider.size.x, -collider.size.y, collider.size.z) * 0.5f);
+        Vertices[3] = collider.transform.TransformPoint(collider.center + new Vector3(-collider.size.x, -collider.size.y, collider.size.z) * 0.5f);
+        Vertices[4] = collider.transform.TransformPoint(collider.center + new Vector3(-collider.size.x, collider.size.y, -collider.size.z) * 0.5f);
+        Vertices[5] = collider.transform.TransformPoint(collider.center + new Vector3(collider.size.x, collider.size.y, -collider.size.z) * 0.5f);
+        Vertices[6] = collider.transform.TransformPoint(collider.center + new Vector3(collider.size.x, collider.size.y, collider.size.z) * 0.5f);
+        Vertices[7] = collider.transform.TransformPoint(collider.center + new Vector3(-collider.size.x, collider.size.y, collider.size.z) * 0.5f);
+
+        Right = rotation * Vector3.right;
+        Up = rotation * Vector3.up;
+        Forward = rotation * Vector3.forward;
+    }
+}
+
+public class ObbCollisionDetection : MonoBehaviour
+{
+   
+
+    public static Obb ToObb(BoxCollider collider)
+    {
+        return new Obb(collider);
+    }
+
+    
+
+    public static bool Intersects(Obb a, Obb b)
+    {
+        if (Separated(a.Vertices, b.Vertices, a.Right))
+            return false;
+        if (Separated(a.Vertices, b.Vertices, a.Up))
+            return false;
+        if (Separated(a.Vertices, b.Vertices, a.Forward))
+            return false;
+
+        if (Separated(a.Vertices, b.Vertices, b.Right))
+            return false;
+        if (Separated(a.Vertices, b.Vertices, b.Up))
+            return false;
+        if (Separated(a.Vertices, b.Vertices, b.Forward))
+            return false;
+
+        if (Separated(a.Vertices, b.Vertices, Vector3.Cross(a.Right, b.Right)))
+            return false;
+        if (Separated(a.Vertices, b.Vertices, Vector3.Cross(a.Right, b.Up)))
+            return false;
+        if (Separated(a.Vertices, b.Vertices, Vector3.Cross(a.Right, b.Forward)))
+            return false;
+
+        if (Separated(a.Vertices, b.Vertices, Vector3.Cross(a.Up, b.Right)))
+            return false;
+        if (Separated(a.Vertices, b.Vertices, Vector3.Cross(a.Up, b.Up)))
+            return false;
+        if (Separated(a.Vertices, b.Vertices, Vector3.Cross(a.Up, b.Forward)))
+            return false;
+
+        if (Separated(a.Vertices, b.Vertices, Vector3.Cross(a.Forward, b.Right)))
+            return false;
+        if (Separated(a.Vertices, b.Vertices, Vector3.Cross(a.Forward, b.Up)))
+            return false;
+        if (Separated(a.Vertices, b.Vertices, Vector3.Cross(a.Forward, b.Forward)))
+            return false;
+
+        return true;
+    }
+
+    static bool Separated(Vector3[] vertsA, Vector3[] vertsB, Vector3 axis)
+    {
+        // Handles the cross product = {0,0,0} case
+        if (axis == Vector3.zero)
+            return false;
+
+        var aMin = float.MaxValue;
+        var aMax = float.MinValue;
+        var bMin = float.MaxValue;
+        var bMax = float.MinValue;
+
+        // Define two intervals, a and b. Calculate their min and max values
+        for (var i = 0; i < 8; i++)
+        {
+            var aDist = Vector3.Dot(vertsA[i], axis);
+            aMin = aDist < aMin ? aDist : aMin;
+            aMax = aDist > aMax ? aDist : aMax;
+            var bDist = Vector3.Dot(vertsB[i], axis);
+            bMin = bDist < bMin ? bDist : bMin;
+            bMax = bDist > bMax ? bDist : bMax;
+        }
+
+        // One-dimensional intersection test between a and b
+        var longSpan = Mathf.Max(aMax, bMax) - Mathf.Min(aMin, bMin);
+        var sumSpan = aMax - aMin + bMax - bMin;
+        return longSpan >= sumSpan; // > to treat touching as intersection
+    }
+}
