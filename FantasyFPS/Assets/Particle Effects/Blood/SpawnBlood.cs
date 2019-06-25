8853d4ead68f5637e8050375896e49bad47bf5ef
szymosi <szymon.osicinski@gmail.com>
2019-06-25 11:04:13 +0200

    Revert "Revert "Merge branch 'ParticleEffects'""
    This reverts commit dc36dac220b4af7650f6563a16d3b3960fe0037f.

---
+++ FantasyFPS/Assets/Particle Effects/Blood/SpawnBlood.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBlood : MonoBehaviour
+{
+    public GameObject blood;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            Instantiate(blood, transform.position, transform.rotation, transform);
+        }
+    }
+}
