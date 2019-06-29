435cc40067967b181b476bda5de443afb088dc4f
szymosi <szymon.osicinski@gmail.com>
2019-06-29 22:23:48 +0200

    Add StepTrap

---
+++ FantasyFPS/Assets/Dungeon/StepTrap/Free medieval weapons/Prefabs/ArrowScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    public float lifetime = 3;
+    private float lifelength = 0;
+    public float speed = 10;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lifelength > lifetime)
+            Destroy(gameObject);
+        else
+            lifelength += Time.deltaTime;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("wall"))
+            Destroy(rb);
+    }
+}
---
+++ FantasyFPS/Assets/Dungeon/StepTrap/Trap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    private double TimePassed=0;
+    public double TimeBetweenShoots = 10;
+    public GameObject Emiter;
+    public GameObject ArrowPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        TimePassed += Time.deltaTime;
+        if(TimePassed>=TimeBetweenShoots)
+        {
+            Shoot();
+            TimePassed = 0;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        TimePassed = 0;
+    }
+    private void Shoot()
+    {
+        Debug.Log("coll");
+        Instantiate(ArrowPrefab, Emiter.transform.position,Emiter.transform.rotation);
+    }
+}
