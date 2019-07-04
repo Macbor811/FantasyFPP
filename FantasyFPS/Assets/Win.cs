9d5480d102aa088eb94fcd9a83a2a60321f438a0
szymosi <szymon.osicinski@gmail.com>
2019-07-04 14:26:47 +0200

    Add win And gameover Script

---
+++ FantasyFPS/Assets/Win.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Win : MonoBehaviour
+{
+    public GameObject winText;
+    private bool win = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(win)
+        {
+            winText.SetActive(true);
+            GetComponent<FirstPersonController>().enabled = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "WinArea")
+            win = true;
+    }
+}
